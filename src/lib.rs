3fa50c1d9b097fda7f7abe8b50aa663c3cf2ccc2
Michal Podhradsky <mpodhradsky@galois.com>
2019-01-11 11:10:14 -0800

    Write/read frame functions

--- src/lib.rs
+++ src/lib.rs
@@ -91,6 +91,13 @@ impl MavFrame {
     }
 }
 
+/// Read whole frame
+#[cfg(feature = "std")]
+pub fn read_frame<R: Read>(r: &mut R) -> Result<MavFrame> {
+    let (header,msg) = read(r)?;
+    Ok(MavFrame{header,msg})
+}
+
 
 /// Read a MAVLink message from a Read stream.
 #[cfg(feature = "std")]
@@ -130,6 +137,12 @@ pub fn read<R: Read>(r: &mut R) -> Result<(MavHeader, MavMessage)> {
             ));
         }
     }
+}
+
+/// Write whole frame
+#[cfg(feature = "std")]
+pub fn write_frame<W: Write>(w: &mut W, frame: &MavFrame) -> Result<()> {
+    write(w, frame.header, &frame.msg)
 }
 
 /// Write a MAVLink message to a Write stream.
