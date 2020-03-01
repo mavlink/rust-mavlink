15dceb181c244da9c5e8ec6c0ac1f6731211fe84
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-03-01 15:31:54 -0500

    it all works! it also no longer compiles on rust 1.40.0 for some reason, but it works fine on rust stable 1.41.1 so i don't think it's a big deal

--- tests/encode_decode_tests.rs
+++ tests/encode_decode_tests.rs
@@ -4,7 +4,7 @@ mod test_shared;
 
 #[cfg(test)]
 mod test_encode_decode {
-    use mavlink::{common, Message};
+    use mavlink::{ardupilotmega, common, Message};
 
     #[test]
     pub fn test_echo_heartbeat() {
@@ -19,7 +19,8 @@ mod test_encode_decode {
         .expect("Failed to write message");
 
         let mut c = v.as_slice();
-        let (_header, recv_msg): (mavlink::MavHeader, common::MavMessage) = mavlink::read_v2_msg(&mut c).expect("Failed to read");
+        let (_header, recv_msg): (mavlink::MavHeader, common::MavMessage) =
+            mavlink::read_v2_msg(&mut c).expect("Failed to read");
         assert_eq!(recv_msg.message_id(), 0);
     }
 
@@ -39,10 +40,7 @@ mod test_encode_decode {
         let (_header, recv_msg) = mavlink::read_v2_msg(&mut c).expect("Failed to read");
 
         if let common::MavMessage::COMMAND_INT(recv_msg) = recv_msg {
-            assert_eq!(
-                recv_msg.command,
-                common::MavCmd::MAV_CMD_NAV_TAKEOFF
-            );
+            assert_eq!(recv_msg.command, common::MavCmd::MAV_CMD_NAV_TAKEOFF);
         } else {
             panic!("Decoded wrong message type")
         }
@@ -67,6 +65,59 @@ mod test_encode_decode {
                 mavlink::common::MavModeFlag::MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
                 recv_msg.mode & mavlink::common::MavModeFlag::MAV_MODE_FLAG_CUSTOM_MODE_ENABLED
             );
+        } else {
+            panic!("Decoded wrong message type")
+        }
+    }
+
+    /// This test makes sure that we can still receive messages in the common set
+    /// properly when we're trying to decode APM messages.
+    #[test]
+    pub fn test_echo_apm_heartbeat() {
+        let mut v = vec![];
+        let send_msg = crate::test_shared::get_heartbeat_msg();
+
+        mavlink::write_v2_msg(
+            &mut v,
+            crate::test_shared::COMMON_MSG_HEADER,
+            &ardupilotmega::MavMessage::common(common::MavMessage::HEARTBEAT(send_msg.clone())),
+        )
+        .expect("Failed to write message");
+
+        let mut c = v.as_slice();
+        let (_header, recv_msg) = mavlink::read_v2_msg(&mut c).expect("Failed to read");
+
+        if let ardupilotmega::MavMessage::common(recv_msg) = recv_msg {
+            match &recv_msg {
+                common::MavMessage::HEARTBEAT(data) => {
+                    assert_eq!(recv_msg.message_id(), 0);
+                }
+                _ => panic!("Decoded wrong message type"),
+            }
+        } else {
+            panic!("Decoded wrong message type")
+        }
+    }
+
+    /// This test makes sure that messages that are not
+    /// in the common set also get encoded and decoded
+    /// properly.
+    #[test]
+    pub fn test_echo_apm_mount_status() {
+        let mut v = vec![];
+        let send_msg = crate::test_shared::get_apm_mount_status();
+
+        mavlink::write_v2_msg(
+            &mut v,
+            crate::test_shared::COMMON_MSG_HEADER,
+            &ardupilotmega::MavMessage::MOUNT_STATUS(send_msg.clone()),
+        )
+        .expect("Failed to write message");
+
+        let mut c = v.as_slice();
+        let (_header, recv_msg) = mavlink::read_v2_msg(&mut c).expect("Failed to read");
+        if let ardupilotmega::MavMessage::MOUNT_STATUS(recv_msg) = recv_msg {
+            assert_eq!(4, recv_msg.pointing_b);
         } else {
             panic!("Decoded wrong message type")
         }
