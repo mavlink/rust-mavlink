cb1ef763dfd34090835cab9bfa7cc9dd13c7d72f
Patrick José Pereira <patrickelectric@gmail.com>
2020-03-11 12:35:11 -0300

    Fix build (#43)
    * Fix implementation of default_message_from_id
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>
    
    * tests: Add helper_test with test for default_message_from_id
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ tests/helper_tests.rs
@@ -0,0 +1,19 @@
+extern crate mavlink;
+
+#[cfg(test)]
+#[cfg(all(feature = "std", feature = "common"))]
+mod helper_tests {
+    use crate::mavlink::{common::MavMessage, Message};
+
+    #[test]
+    fn test_get_default_message_from_id() {
+        let id: std::result::Result<u32, &'static str> = MavMessage::message_id_from_name("PING");
+        let id = id.unwrap();
+        assert!(id == 4, "Invalid id for message name: PING");
+        let message = MavMessage::default_message_from_id(id);
+        match message {
+            Ok(MavMessage::PING(_)) => {},
+            _ => unreachable!("Invalid message type."),
+        }
+    }
+}
