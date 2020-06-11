a3adcd9d08f54134cabb0c416d768a7e80ff712e
Patrick José Pereira <patrickelectric@gmail.com>
2020-06-11 13:14:06 -0300

    travis: Enable backtrace when running tests (#55)
    * travis: Enable backtrace when running tests
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>
    
    * tcp_loopback_tests: Add sleep to wait for TCP server connection
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>
    
    * tcp_loopback_tests: Run cargo fmt
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/tcp_loopback_tests.rs
+++ tests/tcp_loopback_tests.rs
@@ -12,46 +12,44 @@ mod test_tcp_connections {
     pub fn test_tcp_loopback() {
         const RECEIVE_CHECK_COUNT: i32 = 5;
 
-        let server_thread = thread::spawn({
-            move || {
-                //TODO consider using get_available_port to use a random port
-                let server =
-                    mavlink::connect("tcpin:0.0.0.0:14550").expect("Couldn't create server");
+        let server_thread = thread::spawn(move || {
+            //TODO consider using get_available_port to use a random port
+            let server = mavlink::connect("tcpin:0.0.0.0:14550").expect("Couldn't create server");
 
-                let mut recv_count = 0;
-                for _i in 0..RECEIVE_CHECK_COUNT {
-                    match server.recv() {
-                        Ok((_header, msg)) => {
-                            match msg {
-                                mavlink::common::MavMessage::HEARTBEAT(_heartbeat_msg) => {
-                                    recv_count += 1;
-                                }
-                                _ => {
-                                    // one message parse failure fails the test
-                                    break;
-                                }
+            let mut recv_count = 0;
+            for _i in 0..RECEIVE_CHECK_COUNT {
+                match server.recv() {
+                    Ok((_header, msg)) => {
+                        match msg {
+                            mavlink::common::MavMessage::HEARTBEAT(_heartbeat_msg) => {
+                                recv_count += 1;
+                            }
+                            _ => {
+                                // one message parse failure fails the test
+                                break;
                             }
                         }
-                        Err(..) => {
-                            // one message read failure fails the test
-                            break;
-                        }
+                    }
+                    Err(..) => {
+                        // one message read failure fails the test
+                        break;
                     }
                 }
-                assert_eq!(recv_count, RECEIVE_CHECK_COUNT);
             }
+            assert_eq!(recv_count, RECEIVE_CHECK_COUNT);
         });
 
+        // Give some time for the server to connect
+        thread::sleep(std::time::Duration::from_millis(100));
+
         // have the client send a few hearbeats
-        thread::spawn({
-            move || {
-                let msg =
-                    mavlink::common::MavMessage::HEARTBEAT(crate::test_shared::get_heartbeat_msg());
-                let client =
-                    mavlink::connect("tcpout:127.0.0.1:14550").expect("Couldn't create client");
-                for _i in 0..RECEIVE_CHECK_COUNT {
-                    client.send_default(&msg).ok();
-                }
+        thread::spawn(move || {
+            let msg =
+                mavlink::common::MavMessage::HEARTBEAT(crate::test_shared::get_heartbeat_msg());
+            let client =
+                mavlink::connect("tcpout:127.0.0.1:14550").expect("Couldn't create client");
+            for _i in 0..RECEIVE_CHECK_COUNT {
+                client.send_default(&msg).ok();
             }
         });
 
