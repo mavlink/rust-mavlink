142f404e5240699257c404de2e25908ee970c9cc
Patrick José Pereira <patrickelectric@gmail.com>
2020-07-04 20:10:12 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -191,6 +191,81 @@ impl<M: Message> MavFrame<M> {
     }
 }
 
+trait MavlinkPacketFormat {
+    fn checksum(&mut self) -> &u16;
+
+    fn calc_checksum<M: Message>(&self) -> u16;
+
+    fn update_checksum<M: Message>(&mut self) {
+        *self.checksum() = self.calc_checksum::<M>();
+    }
+
+    fn validate_checksum<M: Message>(&self) ->bool {
+        return *self.checksum() == self.calc_checksum::<M>();
+    }
+}
+
+/// MAVLink V1 packet format: https://mavlink.io/en/guide/serialization.html#v1_packet_format
+struct MavlinkV1PacketFormat {
+    pub magic: u8,
+    pub len: u8,
+    pub seq: u8,
+    pub sysid: u8,
+    pub compid: u8,
+    pub msgid: u8,
+    pub payload: Vec<u8>,
+    pub checksum: u16,
+}
+
+impl Default for MavlinkV1PacketFormat {
+    fn default() -> Self {
+        MavlinkV1PacketFormat {
+            magic: MAV_STX,
+            ..Default::default()
+        }
+    }
+}
+
+impl MavlinkPacketFormat for MavlinkV1PacketFormat {
+    fn checksum(&mut self) -> &u16 {
+        &self.checksum
+    }
+
+    fn calc_checksum<M: Message>(&self) -> u16 {
+        let mut crc_calculator = CRCu16::crc16mcrf4cc();
+        crc_calculator.digest(&[self.len, self.seq, self.sysid, self.compid, self.msgid]);
+        crc_calculator.digest(&self.payload);
+        crc_calculator.digest(&[M::extra_crc(self.msgid.into())]);
+
+        return crc_calculator.get_crc();
+    }
+}
+
+/// MAVLink V2 packet format: https://mavlink.io/en/guide/serialization.html#mavlink2_packet_format
+struct MavlinkV2PacketFormat {
+    pub magic: u8,
+    pub len: u8,
+    pub incompat_flags: u8,
+    pub compat_flags: u8,
+    pub seq: u8,
+    pub sysid: u8,
+    pub compid: u8,
+    pub msgid: u8,
+    pub payload: Vec<u8>,
+    pub checksum: u16,
+    pub signature: [u8; 13],
+}
+
+impl Default for MavlinkV2PacketFormat {
+    fn default() -> Self {
+        MavlinkV2PacketFormat {
+            magic: MAV_STX_V2,
+            ..Default::default()
+        }
+    }
+}
+
+/*
 pub fn read_versioned_msg<M: Message, R: Read>(
     r: &mut R,
     version: MavlinkVersion,
@@ -337,6 +412,7 @@ pub fn read_v2_msg<M: Message, R: Read>(
             .map_err(|err| err.into());
     }
 }
+*/
 
 /// Write a message using the given mavlink version
 pub fn write_versioned_msg<M: Message, W: Write>(
