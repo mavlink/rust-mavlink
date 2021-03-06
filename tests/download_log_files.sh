85f5a53689e2455aee88021cf0b4cca79a108bfd
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-06 13:26:02 -0300

    tests: download_log_files: Add fixed version for logs
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++ tests/download_log_files.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+
 if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
-    wget http://autotest.ardupilot.org/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/ArduPlane-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/APMrover2-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    URL=https://autotest.ardupilot.org/history/2021-03-05-16:03/
+    wget ${URL}/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/APMrover2-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
+    wget ${URL}/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
     truncate -s 1M /tmp/testlogs/*.tlog
 else
     echo "log folder already exist."
