9696cf257b7842a5705420fad700c26e033e94aa
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-07 00:20:28 -0300

    bin: mavlink-dump: Use new from for dialects
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/bin/mavlink-dump.rs
+++ src/bin/mavlink-dump.rs
@@ -31,16 +31,16 @@ fn main() {
 
     let vehicle = Arc::new(mavconn);
     vehicle
-        .send(&mavlink::MavHeader::default(), &request_parameters())
+        .send(&mavlink::MavHeader::default(), &request_parameters().into())
         .unwrap();
     vehicle
-        .send(&mavlink::MavHeader::default(), &request_stream())
+        .send(&mavlink::MavHeader::default(), &request_stream().into())
         .unwrap();
 
     thread::spawn({
         let vehicle = vehicle.clone();
         move || loop {
-            let res = vehicle.send_default(&heartbeat_message());
+            let res = vehicle.send_default(&heartbeat_message().into());
             if res.is_ok() {
                 thread::sleep(Duration::from_secs(1));
             } else {
@@ -77,42 +77,34 @@ fn main() {
 /// If only common dialect is used, the `ardupilotmega::MavMessage::common` is not necessary,
 /// and the function could return only a simple `mavlink::common::MavMessage` type
 #[cfg(feature = "std")]
-pub fn heartbeat_message() -> mavlink::ardupilotmega::MavMessage {
-    mavlink::ardupilotmega::MavMessage::common({
-        mavlink::common::MavMessage::HEARTBEAT(mavlink::common::HEARTBEAT_DATA {
-            custom_mode: 0,
-            mavtype: mavlink::common::MavType::MAV_TYPE_QUADROTOR,
-            autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
-            base_mode: mavlink::common::MavModeFlag::empty(),
-            system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
-            mavlink_version: 0x3,
-        })
+pub fn heartbeat_message() -> mavlink::common::MavMessage {
+    mavlink::common::MavMessage::HEARTBEAT(mavlink::common::HEARTBEAT_DATA {
+        custom_mode: 0,
+        mavtype: mavlink::common::MavType::MAV_TYPE_QUADROTOR,
+        autopilot: mavlink::common::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
+        base_mode: mavlink::common::MavModeFlag::empty(),
+        system_status: mavlink::common::MavState::MAV_STATE_STANDBY,
+        mavlink_version: 0x3,
     })
 }
 
 /// Create a message requesting the parameters list
 #[cfg(feature = "std")]
-pub fn request_parameters() -> mavlink::ardupilotmega::MavMessage {
-    mavlink::ardupilotmega::MavMessage::common({
-        mavlink::common::MavMessage::PARAM_REQUEST_LIST(mavlink::common::PARAM_REQUEST_LIST_DATA {
-            target_system: 0,
-            target_component: 0,
-        })
+pub fn request_parameters() -> mavlink::common::MavMessage {
+    mavlink::common::MavMessage::PARAM_REQUEST_LIST(mavlink::common::PARAM_REQUEST_LIST_DATA {
+        target_system: 0,
+        target_component: 0,
     })
 }
 
 /// Create a message enabling data streaming
 #[cfg(feature = "std")]
-pub fn request_stream() -> mavlink::ardupilotmega::MavMessage {
-    mavlink::ardupilotmega::MavMessage::common({
-        mavlink::common::MavMessage::REQUEST_DATA_STREAM(
-            mavlink::common::REQUEST_DATA_STREAM_DATA {
-                target_system: 0,
-                target_component: 0,
-                req_stream_id: 0,
-                req_message_rate: 10,
-                start_stop: 1,
-            },
-        )
+pub fn request_stream() -> mavlink::common::MavMessage {
+    mavlink::common::MavMessage::REQUEST_DATA_STREAM(mavlink::common::REQUEST_DATA_STREAM_DATA {
+        target_system: 0,
+        target_component: 0,
+        req_stream_id: 0,
+        req_message_rate: 10,
+        start_stop: 1,
     })
 }
