64a6d079e0ba9c56e5e62e9dc0785941ae4d3dc6
Patrick José Pereira <patrickelectric@gmail.com>
2021-07-06 17:44:42 -0300

    tests: Update download_log_files
    ArduPilot lost the .tlog files
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++ tests/download_log_files.sh
@@ -3,11 +3,11 @@
 if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
-    URL=https://autotest.ardupilot.org/history/2021-03-05-16:03/
-    wget ${URL}/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
-    wget ${URL}/APMrover2-test.tlog --directory-prefix=/tmp/testlogs/
+    URL=https://autotest.ardupilot.org/history/2021-07-06-00:07
+    #wget ${URL}/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    #wget ${URL}/Rover-test.tlog --directory-prefix=/tmp/testlogs/
     wget ${URL}/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
-    wget ${URL}/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
+    #wget ${URL}/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
     wget ${URL}/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
     wget ${URL}/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
     wget ${URL}/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
