2f95ccd8423b272402c3c88374620dfc041771de
Patrick José Pereira <patrickelectric@gmail.com>
2021-08-25 11:37:47 -0400

    tests: Update download files
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/download_log_files.sh
+++ tests/download_log_files.sh
@@ -3,14 +3,14 @@
 if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
-    URL=https://autotest.ardupilot.org/history/2021-07-06-00:07
+    URL=https://autotest.ardupilot.org/history/2021-08-24-22:08/
     #wget ${URL}/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
     #wget ${URL}/Rover-test.tlog --directory-prefix=/tmp/testlogs/
     wget ${URL}/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
     #wget ${URL}/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
-    wget ${URL}/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
-    wget ${URL}/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
-    wget ${URL}/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    #wget ${URL}/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
+    #wget ${URL}/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
+    #wget ${URL}/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
     truncate -s 1M /tmp/testlogs/*.tlog
 else
     echo "log folder already exist."
