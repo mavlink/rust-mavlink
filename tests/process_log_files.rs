138c2d22775571a117d0be6631ee8b0804760250
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-08 15:55:55 -0300

    tests: process_log_files: Check a single file
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tests/process_log_files.rs
+++ tests/process_log_files.rs
@@ -1,47 +1,31 @@
 extern crate mavlink;
 
 #[cfg(test)]
-#[cfg(ignore)]
 #[cfg(all(feature = "default", feature = "ardupilotmega"))]
 mod process_files {
     use mavlink::ardupilotmega::MavMessage;
     use mavlink::error::MessageReadError;
 
     #[test]
-    pub fn all() {
+    pub fn get_file() {
         // Get path for download script
-        let test_script = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
-            .join("tests/download_log_files.sh")
+        let tlog = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/log.tlog")
             .canonicalize()
             .unwrap();
-        let test_script = test_script.to_str().unwrap();
 
-        // Run download script
-        let result = std::process::Command::new("sh").arg(test_script).output();
-        assert_eq!(result.is_ok(), true);
+        let tlog = tlog.to_str().unwrap();
 
-        // Grab all log files
-        let test_path = "/tmp/testlogs/";
-        let files = std::fs::read_dir(test_path).unwrap();
-        let mut files = files
-            .filter_map(Result::ok)
-            .filter(|d| d.path().extension().unwrap_or_default().to_str() == Some("tlog"));
 
-        while let Some(file) = files.next() {
-            let filename = file.file_name();
-            let filename = filename.to_str().unwrap();
-            let filename = std::path::Path::new(test_path)
-                .join(filename)
-                .canonicalize()
-                .unwrap();
-            let filename = filename.to_str().unwrap();
+        let filename = std::path::Path::new(tlog);
+        let filename = filename.to_str().unwrap();
+        dbg!(filename);
 
-            println!("Processing file: {}", &filename);
-            let connection_string = format!("file:{}", &filename);
+        println!("Processing file: {}", &filename);
+        let connection_string = format!("file:{}", &filename);
 
-            // Process file
-            process_file(&connection_string);
-        }
+        // Process file
+        process_file(&connection_string);
     }
 
     pub fn process_file(connection_string: &str) {
@@ -52,7 +36,7 @@ mod process_files {
         let mut counter = 0;
         loop {
             match vehicle.recv() {
-                Ok((_header, msg)) => {
+                Ok((_header, _msg)) => {
                     counter += 1;
                 }
                 Err(MessageReadError::Io(e)) => match e.kind() {
@@ -70,7 +54,7 @@ mod process_files {
 
         println!("Number of parsed messages: {}", counter);
         assert!(
-            counter > 11128,
+            counter == 1374,
             "Unable to hit the necessary amount of matches"
         );
     }
