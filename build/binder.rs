d59cd046e0071c2919792f6ea3cfccefd8f0dca5
Valdemar Erk <valdemar@erk.dev>
2022-08-18 16:33:36 +0200

    Update the parser to quote 1.0
    This patch updates the parser from using quote 0.3 to using quote 1.0
    and proc_macro2.

--- build/binder.rs
+++ build/binder.rs
@@ -1,9 +1,9 @@
-use quote::Ident;
+use quote::format_ident;
 use std::io::Write;
 
 pub fn generate<W: Write>(modules: Vec<String>, out: &mut W) {
     let modules_tokens = modules.into_iter().map(|module| {
-        let module_ident = Ident::from(module.clone());
+        let module_ident = format_ident!("{}", module);
 
         quote! {
             #[allow(non_camel_case_types)]
