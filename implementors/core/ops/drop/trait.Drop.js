483a28add6f998737d9ce5cc20c1d76a8cc5ac65
patrickelectric <patrickelectric@users.noreply.github.com>
2024-01-03 20:00:51 +0000

    deploy: 19154d84d2336a2ce33e529fb1e32b032b51f703

--- implementors/core/ops/drop/trait.Drop.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"serial_unix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"serial_unix/struct.TTYPort.html\" title=\"struct serial_unix::TTYPort\">TTYPort</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
