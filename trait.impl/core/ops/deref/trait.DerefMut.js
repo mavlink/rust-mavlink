588efbbc50c2978e6e2f1a32523925c73d7e89f0
patrickelectric <patrickelectric@users.noreply.github.com>
2024-08-04 17:37:11 +0000

    deploy: 63b6af9fe59eb5cdea6aa385d0c3f4f3d2f2303b

--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"termios":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"termios/struct.Termios.html\" title=\"struct termios::Termios\">Termios</a>"]]
+"termios":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"termios/struct.Termios.html\" title=\"struct termios::Termios\">Termios</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
