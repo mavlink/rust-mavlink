2e749b770d9814c855bfc96601336164b3ca5e49
onur-ozkan <onur-ozkan@users.noreply.github.com>
2025-07-02 05:21:38 +0000

    deploy: aeac5d9d57e0402c2188fa88509cc86a481c6e1e

--- trait.impl/core/fmt/trait.Write.js
+++ trait.impl/core/fmt/trait.Write.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
+    var implementors = Object.fromEntries([["arrayvec",[["impl&lt;const CAP: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]]],["bytes",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[257]}
+//{"start":57,"fragment_lengths":[405,258]}
