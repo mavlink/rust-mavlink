0eba94ee3ba02ef7fb2c81fa9a8a0aa4cc6188f3
JoãoFRF <joaofilipe.1998@gmail.com>
2025-08-22 19:44:18 +0100

    Added tests to tcp and udp both sync and async. Added a sync test for file and changed test file name. Also removed tcp feature from file tests and moved tokio-1 to the async test only.

--- mavlink/tests/udp_loopback_async_tests.rs
+++ mavlink/tests/udp_loopback_async_tests.rs
@@ -2,6 +2,7 @@ mod test_shared;
 
 #[cfg(all(feature = "tokio-1", feature = "udp", feature = "common"))]
 mod test_udp_connections {
+    use mavlink::MessageData;
 
     /// Test whether we can send a message via UDP and receive it OK using async_connect
     #[tokio::test]
@@ -32,6 +33,50 @@ mod test_udp_connections {
             match server.recv().await {
                 Ok((_header, msg)) => {
                     if let mavlink::common::MavMessage::HEARTBEAT(_heartbeat_msg) = msg {
+                        recv_count += 1;
+                    } else {
+                        // one message parse failure fails the test
+                        break;
+                    }
+                }
+                Err(..) => {
+                    // one message read failure fails the test
+                    break;
+                }
+            }
+        }
+        assert_eq!(recv_count, RECEIVE_CHECK_COUNT);
+    }
+
+    /// Test whether we can send a message via UDP and receive it OK using async_connect recv_raw
+    #[tokio::test]
+    pub async fn test_udp_loopback_recv_raw() {
+        const RECEIVE_CHECK_COUNT: i32 = 3;
+
+        let server = mavlink::connect_async::<mavlink::common::MavMessage>("udpin:0.0.0.0:14562")
+            .await
+            .expect("Couldn't create server");
+
+        // have the client send one heartbeat per second
+        tokio::spawn({
+            async move {
+                let msg =
+                    mavlink::common::MavMessage::HEARTBEAT(crate::test_shared::get_heartbeat_msg());
+                let client = mavlink::connect_async("udpout:127.0.0.1:14562")
+                    .await
+                    .expect("Couldn't create client");
+                loop {
+                    client.send_default(&msg).await.ok();
+                }
+            }
+        });
+
+        //TODO use std::sync::WaitTimeoutResult to timeout ourselves if recv fails?
+        let mut recv_count = 0;
+        for _i in 0..RECEIVE_CHECK_COUNT {
+            match server.recv_raw().await {
+                Ok(message) => {
+                    if message.message_id() == mavlink::common::HEARTBEAT_DATA::ID {
                         recv_count += 1;
                     } else {
                         // one message parse failure fails the test
