633b520c8228643b16b1cc03a90ee208b454866d
patrickelectric <patrickelectric@users.noreply.github.com>
2025-12-13 12:15:09 +0000

    deploy: 39bde189f16ea64f7e67851a6aefeee59b5cde5d

--- trait.impl/core/ops/deref/trait.Deref.js
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["mavlink_core",[["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mavlink_core/types/struct.CharArray.html\" title=\"struct mavlink_core::types::CharArray\">CharArray</a>&lt;N&gt;"]]]]);
+    const implementors = Object.fromEntries([["mavlink_core",[["impl&lt;const N: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"mavlink_core/types/struct.CharArray.html\" title=\"struct mavlink_core::types::CharArray\">CharArray</a>&lt;N&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[433]}
+//{"start":59,"fragment_lengths":[435]}
