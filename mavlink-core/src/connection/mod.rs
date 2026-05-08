832a6170885305c4602514e5172df3496b403085
Onur Özkan <work@onurozkan.dev>
2026-05-08 10:01:44 +0300

    bless cargo fmt
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- mavlink-core/src/connection/mod.rs
+++ mavlink-core/src/connection/mod.rs
@@ -17,12 +17,12 @@ pub use sync::{Connectable, Connection, MavConnection, connect};
 /// Returns the socket address for the given address.
 #[cfg(any(feature = "transport-tcp", feature = "transport-udp"))]
 pub(crate) fn get_socket_addr<T: std::net::ToSocketAddrs>(
-	address: &T,
+    address: &T,
 ) -> Result<std::net::SocketAddr, io::Error> {
-	address
-		.to_socket_addrs()?
-		.next()
-		.ok_or(io::Error::other("Host address lookup failed"))
+    address
+        .to_socket_addrs()?
+        .next()
+        .ok_or(io::Error::other("Host address lookup failed"))
 }
 
 #[cfg(feature = "tokio")]
